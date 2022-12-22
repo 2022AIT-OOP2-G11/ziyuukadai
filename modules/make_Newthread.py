b8468f368217b9c104942a27722d776ff5fdf07c
k20235nagata <miru10yuzu@gmail.com>
2022-12-22 19:09:52 +0900

    DB作成場所変更
    DBの作成する場所をDBファイルになるように変更しました

--- modules/make_Newthread.py
+++ modules/make_Newthread.py
@@ -3,7 +3,7 @@ import sqlite3  #DB使用のためのimport文
 def new_thread(Thread_Name, Make_User_Name):
     
     # DB接続。ファイルがなければ作成する
-    con = sqlite3.connect('Thread.db')
+    con = sqlite3.connect('./DB/Thread.db')
 
 
 
