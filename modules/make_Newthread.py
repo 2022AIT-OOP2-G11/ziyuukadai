af4c3d94328389b6d7f30a72df413e4ff8a09ae4
k20235nagata <miru10yuzu@gmail.com>
2022-12-22 19:05:42 +0900

    self削除
    使用しないため削除

--- modules/make_Newthread.py
+++ modules/make_Newthread.py
@@ -1,6 +1,6 @@
 import sqlite3  #DB使用のためのimport文
 
-def new_thread(self, Thread_Name, Make_User_Name):
+def new_thread(Thread_Name, Make_User_Name):
     
     # DB接続。ファイルがなければ作成する
     con = sqlite3.connect('Thread.db')
