5e8412d7ebc1e3fe68a544d02d89117d1b4a82ed
k20235nagata <miru10yuzu@gmail.com>
2022-12-22 16:17:06 +0900

    スレッド新規作成
    スレッドの新規作成の基礎プログラム

---
+++ modules/make_Newthread.py
@@ -0,0 +1,12 @@
+import sqlite3  #DB使用のためのimport文
+
+def new_thread(self, Thread_Name, Make_User_Name):
+    
+    # DB接続。ファイルがなければ作成する
+    con = sqlite3.connect('Thread.db')
+
+
+
+
+if __name__ == "__main__":
+    pass
