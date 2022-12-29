7ed34116873f4c065285f7cc151ccd5532267264
k20235nagata <miru10yuzu@gmail.com>
2022-12-29 14:35:58 +0900

    変更
    DBに2個以上のデータの追加ができていなかったので修正しました。

--- modules/make_Newthread.py
+++ modules/make_Newthread.py
@@ -5,22 +5,32 @@ def new_thread(Thread_Name, Make_User_Name):
     # DB接続。ファイルがなければ作成する
     con = sqlite3.connect('./DB/Thread.db')
 
-    #スレッドの数を確認
-    c = con.execute("SELECT * FROM sqlite_master WHERE type='table'")
-    count = len(c.fetchall())
+    #テーブル(表)があるか確認
+    table_count = con.execute("SELECT * FROM sqlite_master WHERE type='table'")
 
-    if not count: #一つもなかったらテーブルを作成して追加
-        #新規作成した時スレッドIDが0のため1を代入
-        count = 1
+    if table_count: #なかったらテーブルを作成して追加
+        
         #テーブル作成SQL文
         con.execute("CREATE TABLE スレッド一覧(スレッドID INTEGER PRIMARY KEY, スレッド名 STRING" +
                         ", ユーザー名 STRING, 最終更新時間 TIMESTAMP, スレッドを立てた時間 TIMESTAMP)")
+
+        #新規作成した時スレッドIDが0のため1を代入
+        count = 1
+
         #テーブル追加SQL文
         con.execute("INSERT INTO スレッド一覧(スレッドID, スレッド名, ユーザー名, 最終更新時間, スレッドを立てた時間)" +
                         f" values('{count}', '{Thread_Name}', '{Make_User_Name}' ,datetime('now', 'localtime')" +
                             ", datetime('now', 'localtime'))")
+
     else: #あったらスレッドの個数を取得して追加する
-        count += 1#一番高いスレッドIDを作成
+
+        #スレッドの数を確認
+        thread_count = con.execute("SELECT count(スレッドID) FROM スレッド一覧")
+        count = int(thread_count.fetchone()[0])
+
+        #一番高いスレッドIDを作成
+        count += 1
+        
         #テーブル追加SQL文
         con.execute("INSERT INTO スレッド一覧(スレッドID, スレッド名, ユーザー名, 最終更新時間, スレッドを立てた時間)" +
                         f" values('{count}', '{Thread_Name}', '{Make_User_Name}' ,datetime('now', 'localtime')" +
@@ -28,10 +38,35 @@ def new_thread(Thread_Name, Make_User_Name):
 
 
     con.commit()
+
+
+def Get_Thread_All():
+
+    # DB接続。ファイルがなければ作成する
+    con = sqlite3.connect('./DB/Thread.db')
+
+    #スレッドの数を確認
+    c = con.execute("SELECT count(スレッドID) FROM スレッド一覧")
+
+    # print(c.fetchone()[0])
+
+    count = c.fetchone()[0]
+
+
+
+    print(count)
+
+
+
+
+
+
+
     
 
 
 
 
 if __name__ == "__main__":
-    new_thread(Thread_Name="愛工大", Make_User_Name="tomo")
+    new_thread(Thread_Name="c", Make_User_Name="tomo")
+    # Get_Thread_All()
