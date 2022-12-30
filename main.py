8814bd1f6ae57b53e7f4ba58e999016160bda497
makim0939 <95673492+makim0939@users.noreply.github.com>
2022-12-31 00:11:37 +0900

    ルーティングの処理を追加しました。

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask, request ,render_template  # Flaskは必須、requestはリクエストパラメータを処理する場合に使用します。
+from flask import Flask, request ,render_template, redirect  # Flaskは必須、requestはリクエストパラメータを処理する場合に使用します。
+from modules.make_Newthread import new_thread, Get_Thread_All, Get_Thread_One, dictionary, Update_Thread_Time, Delete_One_Thread
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template('test.html')
+    if request.method == "GET":
+       #GETだったら全部のデータベースを取得してindex.htmlに送る
+        
+        return render_template('index.html', threads=)
 
+    elif request.method == "POST":
+        #POSTだったらデータを受け取って、データベースに保存する
+        
+        return redirect("/")
 
 
 if __name__ == '__main__':
