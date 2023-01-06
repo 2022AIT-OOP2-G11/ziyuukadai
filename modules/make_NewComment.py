8ceb4cd94196bea45c6d1be73b9234c30a24c002
Yuri-0708 <yuuri2313@gmail.com>
2023-01-06 21:42:54 +0900

    コメントの追加

--- modules/make_NewComment.py
+++ modules/make_NewComment.py
@@ -8,15 +8,15 @@ import json
 # comment_add -> コメントを追加(引数 : スレッドid, 内容, ユーザー名)
 # comment_get_id -> スレッドidに応じたスレッドの内容をjsonファイルへ保存(引数 : thread_id)
 
-content_db = './DB/content.db'
+content_db = './DB/content.db' #DBの保存場所
 
 #指定されたスレッドのDBに接続し、テーブルが作成されていない場合作成
 def connect_db():
-    con = sqlite3.connect(content_db)
+    con = sqlite3.connect(content_db) #DBに接続
 
     table_count = con.execute("SELECT count(*) FROM sqlite_master WHERE type='table'").fetchone()[0]
-    print(table_count)
 
+    #テーブルが作成されていない場合は作成
     if table_count == 0:
         con.execute("CREATE TABLE コメント(id INTEGER PRIMARY KEY AUTOINCREMENT, スレッドid INTEGER NOT NULL, 内容 TEXT NOT NULL, ユーザー名 TEXT, 投稿時間 TIMESTAMP)")
 
@@ -24,8 +24,9 @@ def connect_db():
 def comment_add(thread_id, content, user_name): 
     con = sqlite3.connect(content_db)
     
+    #DBにデータを保存
     con.execute("INSERT INTO コメント(スレッドid, 内容, ユーザー名, 投稿時間)" +  f"values('{thread_id}', '{content}', '{user_name}', datetime('now','localtime'))")
-    print("登録完了")
+
     con.commit()
     con.close()
 
@@ -35,12 +36,15 @@ def comment_get_id(thread_id):
 
     thread_content = con.execute(f"SELECT * FROM コメント WHERE スレッドid = {thread_id}").fetchall()
 
+    #jsonデータのkeyを設定
     dict_item = ["id", "スレッドid", "内容", "ユーザー名", "投稿時間"]
     result = []
 
+    #{key : item}の形式で辞書型リストに格納
     for item in thread_content:
         result.append(dict(zip(dict_item, item)))
 
+    #jsonファイルに書き込み
     with open('./json/thread_id_content.json', 'w') as f:
         json.dump(result, f, indent=4, ensure_ascii=False)
 
@@ -48,12 +52,6 @@ def comment_get_id(thread_id):
     con.commit()
     con.close()
 
-
-
-if __name__ == "__main__":
-    connect_db()
-    comment_add(1, "今日はいい天気", "Yuri")
-    comment_get_id(1)
 
 
 
