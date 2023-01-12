c5414bcd295c0b7ad683908f2b30ffd143fc8a27
Yuri-0708 <yuuri2313@gmail.com>
2023-01-12 14:06:10 +0900

    Userデータベースの操作を行うメソッド追加

--- modules/make_User.py
+++ modules/make_User.py
@@ -34,15 +34,91 @@ def user_add(username, student_number, password):
     con.commit()
     con.close()
 
+def dictionary(results: list):
+    #辞書型の鍵の配列
+    dict_item = ["ユーザー名", "学籍番号", "パスワード"]
+    array = []
+
+    #先頭のスレッドIDを取得して削除
+    id = results.pop(0)
+
+    #[スレッドID, 内容]の配列を作成
+    array.extend([id, dict(zip(dict_item, results))])
+
+    return array
+
 #全てのユーザを取得
 def get_all_users():
     con = connect_db()
 
+    all_users = con.execute(f"SELECT * FROM ユーザー").fetchall()
+
+    results = []
+    for i in all_users:
+        results.append(dictionary(list(i)))
+
+    #スレッドIDを鍵とした辞書型を作成
+    one_results = dict(results)
+
+    #jsonファイル作成
+    with(open('./json/All_Users.json','w')) as f:
+        json.dump(one_results, f, indent=4, ensure_ascii=False)
+
+    con.commit()
+
+    con.close()
+
+
+
 
 #idからユーザを取得
+def get_id_by_user(id):
+    con = connect_db()
+
+    id_by_users = con.execute(f"SELECT * FROM ユーザー WHERE id = {id}").fetchone()
+
+    results = []
+    if id_by_users is not None:
+        results.append(dictionary(list(id_by_users)))
+
+    #スレッドIDを鍵とした辞書型を作成
+    one_results = dict(results)
+
+    #jsonファイル作成
+    with(open('./json/Id_by_Users.json','w')) as f:
+        json.dump(one_results, f, indent=4, ensure_ascii=False)
+
+    con.commit()
+
+    con.close()
+
+
+
 
 
 #学籍番号からユーザを取得
+def get_studentnumber_by_user(student_number):
+    con = connect_db()
+
+    studentnumber_by_users = con.execute(f"SELECT * FROM ユーザー WHERE 学籍番号 = '{student_number}'").fetchone()
+    # print(studentnumber_by_users)
+
+    results = []
+
+    if studentnumber_by_users is not None:
+        results.append(dictionary(list(studentnumber_by_users)))
+
+    #スレッドIDを鍵とした辞書型を作成
+    one_results = dict(results)
+
+    #jsonファイル作成
+    with(open('./json/Num_by_Users.json','w')) as f:
+        json.dump(one_results, f, indent=4, ensure_ascii=False)
+
+    con.commit()
+
+    con.close()
+
 
 
 
